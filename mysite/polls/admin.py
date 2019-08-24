e796fdfa0050754d4789a8dc7b92084e00632b12
Jawed Khalil <jawed_marzee@outlook.com>
2019-08-24 12:02:20 +0430

    customizing the admin form

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from .models import Choice, Question
 from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+
 
 # Register your models here.
